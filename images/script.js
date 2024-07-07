4171252279a9c068c30120c84196d10ebf647a64
saipolam <147738104+saipolam@users.noreply.github.com>
2024-07-07 15:07:26 +0530

    Create script.js

---
+++ images/script.js
@@ -0,0 +1,19 @@
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function enlargeImage(img) {
+    var modal = document.getElementById("image-modal");
+    var modalImg = document.getElementById("modal-image");
+    var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+}
+
+function closeModal() {
+    var modal = document.getElementById("image-modal");
+    modal.style.display = "none";
+}
